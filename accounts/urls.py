fa845b075f3a8bd0be2eb35a1d9ab3675293ef61
GG <georgia@stormbox.com.au>
2024-10-29 11:07:08 +0800

    started reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import profile_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('profile/', profile_view, name='profile'), 
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
